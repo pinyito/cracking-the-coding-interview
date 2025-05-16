010f8911c312db086070b9f33de3ae03588698c2
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2025-05-15 22:50:38 -0500

    Implemented Publisher, Subscription, and Subscriber for demo

---
+++ reactive-programming-playground/src/main/java/com/thinkpert/sec01/publisher/SubscriptionImpl.java
@@ -0,0 +1,45 @@
+package com.thinkpert.sec01.publisher;
+
+import com.github.javafaker.Faker;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SubscriptionImpl implements Subscription {
+
+    private static final Logger log = LoggerFactory.getLogger(SubscriptionImpl.class);
+    private static final int MAX_ITEMS = 10;
+    private final Faker faker;
+    private final Subscriber<? super String> subscriber;
+    private boolean isCancelled;
+    private int count = 0;
+
+    public SubscriptionImpl(Subscriber<? super String> subscriber) {
+        this.subscriber = subscriber;
+        this.faker = Faker.instance();
+    }
+
+    @Override
+    public void request(long requested) {
+        if(isCancelled){
+            return;
+        }
+        log.info("Subscriber has requested {} items", requested);
+        for(int i = 0; i < requested && count < MAX_ITEMS; i++){
+            count++;
+            this.subscriber.onNext(this.faker.internet().emailAddress());
+        }
+        if(count == MAX_ITEMS){
+            log.info("No more date to produce");
+            this.subscriber.onComplete();
+            this.isCancelled = true;
+        }
+    }
+
+    @Override
+    public void cancel() {
+        log.info("Subscription cancelled");
+        this.isCancelled = true;
+    }
+}
